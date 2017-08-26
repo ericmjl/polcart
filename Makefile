ca919f466aa33b8c8da7fb70c442c1c137edc84f
Eric Ma <ericmajinglong@gmail.com>
2017-08-26 10:51:43 -0400

    updated polcart with pinned hypothesis version

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
 test:
 	py.test --cov --cov-report term-missing -v
+
+release:
+	# rm dist/*
+	python setup.py sdist bdist_wheel
+	twine upload dist/*
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('test_requirements.txt') as testreq_file:
 
 setup(
     name='polcart',
-    version='2017.01.16.14.45',
+    version='2017.08.26.10.50',
     description="A small utility for converting between polar\
 and cartesian units.",
     long_description=readme,
